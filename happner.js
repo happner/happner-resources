f0249f70f3268d10f47c15d37f91a073fabcaff7
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2015-09-28 23:16:36 +0200

    updated config autoloader

--- happner.js
+++ happner.js
@@ -1,19 +1,5 @@
 module.exports = {
   configs: {
-    "autoload":[{
-      "component":{
-        name: "happner-resources",
-        config: {
-          schema:{
-            "exclusive":false
-          },
-          web:{
-            routes:{
-             "static":"resources"
-            } 
-          } 
-        }
-      }
-    }]
+    "autoload":[]
   }
 }
