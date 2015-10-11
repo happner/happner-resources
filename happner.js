01154c5ae12012d9b8611b40a15a2bfa9c1366d2
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2015-10-11 12:00:59 +0200

    bumped version

--- happner.js
+++ happner.js
@@ -1,5 +1,29 @@
 module.exports = {
   configs: {
-    "autoload":[]
+    "autoload":[
+     {
+        "module":{
+          name:"happner-resources",
+          config:{
+            path:__dirname + "/index.js"
+          }
+        },
+        "component":{
+          name:"happner-resources",
+          moduleName: "happner-resources",
+          config: {
+            startMethod:"initialize",
+            schema:{
+              "exclusive":false
+            },
+            web:{
+              routes:{
+                "static":__dirname + "/resources"
+              }
+            }
+          }
+        }
+      }
+    ]
   }
 }
